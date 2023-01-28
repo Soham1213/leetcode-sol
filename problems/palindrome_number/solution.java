0bf513ab0b939276ff44a3748b5c4131048ac1d9
Soham Bihari <76133731+Soham1213@users.noreply.github.com>
2023-01-28 00:01:25 +0000

    Sync LeetCode submission - Palindrome Number (java)

---
+++ problems/palindrome_number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String num = Integer.toString(x);
+        
+        int start = 0;
+        int end = num.length() - 1;
+
+        while (start <= end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            } 
+            start++;
+            end--;
+        }
+        return true;
+    }   
+}
